542061f0b4b38c125ae14a22081306649c9a45f5
TeLiNi <joaotelini03@gmail.com>
2024-12-28 22:07:45 -0300

    verify token existing

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 import db from "./firebase/firebase-config.js";
 import jwt from "jsonwebtoken";
+import { isTokenExpired, getToken, removeToken } from "./utils/tokenVerify.js";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS, 10);
 const SECRETKEY = process.env.SECRETKEY;
 
@@ -29,46 +30,6 @@ app.get('/users', async (req, res) => {
   }
 });
 
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const ref = db.ref("api-time-clock/users");
-
-  try {
-    if (!email || !password) {
-      return res.status(400).json({ message: "Todos os campos devem ser preenchidos." });
-    }
-
-    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    
-    if (!regex.test(email)) {
-      return res.status(400).json({ message: "Email inválido." });
-    }
-
-    const snapshot = await ref.orderByChild("email").equalTo(email).once("value");
-
-    if (!snapshot.exists()) {
-      return res.status(401).json({ message: "Credenciais inválidas." });
-    }
-
-    const userKey = Object.keys(snapshot.val())[0];
-    const user = snapshot.val()[userKey];
-
-    const passValid = await bcrypt.compare(password, user.password);
-
-    if (!passValid) {
-      return res.status(401).json({ message: "Credenciais inválidas." });
-    }
-
-    const token = jwt.sign({ id: userKey }, SECRETKEY, { expiresIn: "1h" });
-
-    res.status(200).json({ message: "Login efetuado com sucesso.", token });
-
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao fazer login', details: error });
-  }
-
-})
-
 // Buscar usuário por ID
 app.get("/users/:id", async (req, res) => {
   const userId = req.params.id;
@@ -87,11 +48,64 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const ref = db.ref("api-time-clock/users");
+
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "Todos os campos devem ser preenchidos." });
+    }
+
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (!regex.test(email)) {
+      return res.status(400).json({ message: "Email inválido." });
+    }
+
+    const snapshot = await ref.orderByChild("email").equalTo(email).once("value");
+
+    if (!snapshot.exists()) {
+      return res.status(401).json({ message: "Credenciais inválidas." });
+    }
+
+    const userKey = Object.keys(snapshot.val())[0];
+    const user = snapshot.val()[userKey];
+
+    const passValid = await bcrypt.compare(password, user.password).catch(() => {
+      return res.status(401).json({ message: "Credenciais inválidas" });
+    });
+
+    if (!passValid) {
+      return res.status(401).json({ message: "Credenciais inválidas." });
+    }
+
+    // Obter o token do banco de dados
+    const token = await getToken(userKey);  // Passar o userKey aqui
+
+    // Verificar se o token está expirado
+    if (isTokenExpired(token)) {
+      await removeToken(userKey);  // Remover o token expirado
+    }
+
+    // Gerar novo token JWT
+    const newToken = jwt.sign({ id: userKey }, SECRETKEY, { expiresIn: 50 });
+
+    // Atualizar o banco de dados com o novo token
+    await db.ref(`api-time-clock/users/${userKey}/token`).set(newToken);  // Alterado aqui
+
+    res.status(200).json({ message: "Login efetuado com sucesso.", token: newToken });
+
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao fazer login', details: error });
+  }
+});
+
 // Criar usuário
 app.post('/users', async (req, res) => {
   const { name, email, password, role } = req.body;
   const ref = db.ref("api-time-clock/users");
-  const trimmedRole = role.trim()
+  const trimmedRole = role.trim();
 
   try {
     if (!name || !email || !password || !trimmedRole) {
@@ -99,13 +113,13 @@ app.post('/users', async (req, res) => {
     }
 
     let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    
+
     if (!regex.test(email)) {
       return res.status(400).json({ message: "Email inválido." });
     }
-    
+
     const snapshot = await ref.orderByChild("email").equalTo(email).once("value");
-    
+
     if (snapshot.exists()) {
       return res.status(409).json({ message: "Usuário com esse email já cadastrado." });
     }
@@ -121,7 +135,7 @@ app.post('/users', async (req, res) => {
     }
 
     // Gerar um ID único para o usuário com push()
-    const userRef = ref.push(); 
+    const userRef = ref.push();
 
     await userRef.set({ name, email, password: hash, role });
 
@@ -141,7 +155,7 @@ app.delete('/users/:id', async (req, res) => {
     const snapshot = await ref.once("value");
 
     if (snapshot.exists()) {
-      await ref.remove(); 
+      await ref.remove();
       res.status(200).json({ message: `Usuário ${id} removido com sucesso!` })
     } else {
       res.status(400).json({ message: "ID não encontrado." });
@@ -153,5 +167,4 @@ app.delete('/users/:id', async (req, res) => {
 
 app.listen(port, () => {
   console.log(`Servidor rodando em: http://localhost:${port}`);
-
 });
