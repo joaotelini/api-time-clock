6c41b36151b8072a78b0846a2d3540d6745c3295
TeLiNi <joaotelini03@gmail.com>
2025-01-09 21:12:26 -0300

    change port number

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import { isTokenExpired, getToken, removeToken } from "./utils/tokenVerify.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS, 10);
 const SECRETKEY = process.env.SECRETKEY;
 
