ede77745ead1cd7497b3d676522ddef5c99bbe68
João Pedro Telini <joaotelini03@gmail.com>
2025-01-23 22:45:04 -0300

    code improvement

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,14 +1,20 @@
 import admin from 'firebase-admin';
 import dotenv from 'dotenv';
+import { readFile } from 'fs/promises';
 
 dotenv.config();
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY)
+const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+// Leia e parseie o JSON de forma assíncrona
+const serviceAccount = JSON.parse(
+  await readFile(new URL(serviceAccountPath, import.meta.url))
+);
 
 // Inicializa o Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://api-time-clock-6244f-default-rtdb.firebaseio.com",
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
 // Exporta o banco de dados para ser utilizado em outros lugares
